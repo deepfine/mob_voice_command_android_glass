de82dfced8c0af59fedc70e8c0fa3e00b0a3ae51
Hyechan Wang <wangoose@naver.com>
2026-06-23 17:58:49 +0900

    [DF-236] feat($realwear): add RealWear Navigator Z1 model

--- voicecommand/src/main/kotlin/com/deepfine/voicecommand/utils/DeviceTypeUtil.kt
+++ voicecommand/src/main/kotlin/com/deepfine/voicecommand/utils/DeviceTypeUtil.kt
@@ -7,7 +7,7 @@ internal object DeviceTypeUtil {
   val deviceModel = Build.MODEL.uppercase()
 
   private val vuzixModels = setOf("BLADE 2", "M400", "M4000")
-  private val realWearModels = setOf("T1100G", "T1200G", "T21G", "A31G")
+  private val realWearModels = setOf("T1100G", "T1200G", "T21G", "T21S", "A31G")
 
   private val isVuzix: Boolean = vuzixModels.any { deviceModel.contains(it) }
 
